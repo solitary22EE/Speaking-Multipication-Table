5a56ac22d3081cefc864e1b151e6f07a0aa0abcb
solitary <rajkumarkarikkal8942@gmail.com>
2023-03-17 17:53:20 +0530

    This is my first git program

---
+++ hai.py
@@ -0,0 +1,24 @@
+import pyttsx3
+
+n = input("Enter the number  ")
+
+dic = {
+  "1" : "ones",
+  "2" : "twos",
+  "3" : "threes",
+  "4" : "fours",
+  "5" : "sixs",
+  "7" : "sevens",
+  "8" : "eights",
+  "9" : "nines",
+  "10" : "tens"
+} 
+txt =""
+for i in range( 1,11):
+    mul = i*int(n)
+    txt +=str(i) +" "+ dic[n] +" are  "+str(mul) + "\n"
+    print(txt)
+    #engine = pyttsx3.init()
+    #engine.say(txt)
+    #engine.runAndWait()
+   
